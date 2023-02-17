1d8f6c4817c9ba91d52a3c3e3a5308fe5a3ebd31
EmoHei <98215816+EmoHei@users.noreply.github.com>
2023-02-17 14:21:26 +0100

    add not found  page and forgot password page and functuality

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import './App.scss';
 import HeaderComp from './components/HeaderComp';
 import PrivateRoute from './components/PrivateRoute';
 import CreatePage from './pages/CreatePage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage'
+import NotFound from './pages/NotFound';
 import RegisterPage from './pages/RegisterPage'
 function App() {
   return (
@@ -16,46 +19,22 @@ function App() {
         <Routes>
 
           <Route path="/" element={<HomePage></HomePage>} />
-          {/* <Route path='/profile' element={<PrivateRoute />}>
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-          <Route path="/update/:id" element={<Edit />} /> */}
 
-         <Route path="/login" element={<LoginPage></LoginPage>} />
-       
+          <Route path="/login" element={<LoginPage></LoginPage>} />
           <Route path="/register" element={<RegisterPage></RegisterPage>} />
+
           <Route path="/create" element={<PrivateRoute />} >
             <Route path="/create" element={<CreatePage></CreatePage>} />
           </Route>
-
-          {/* <Route path="/forgot-password" element={<ForgotPassword></ForgotPassword>} /> */}
-
-          {/* <Route path='/' element={<PrivateRoute />}>
-            <Route path="/offers" element={<Offers></Offers>} />
-          </Route>
-          <Route path='/category/:categoryName' element={<PrivateRoute />}>
-            <Route path="/category/:categoryName" element={<Category></Category>} />
-          </Route>
-          <Route path='/category/:categoryName/:listingId' element={<PrivateRoute />}>
-            <Route path="/category/:categoryName/:listingId" element={<Listing></Listing>} />
-          </Route>
-
-          <Route path="/create" element={<PrivateRoute />} >
-            <Route path="/create" element={<Create></Create>} />
-          </Route>
-
-          <Route path="edit" element={<PrivateRoute />}>
-            <Route path="/edit/:listingId" element={<Edit />} />
-          </Route>
-
+         
           <Route path="/logout" element={<PrivateRoute />} >
             <Route path="/logout" element={<HomePage></HomePage>} />
           </Route>
-          <Route path="edit-listing" element={<PrivateRoute />}>
-            <Route path="/edit-listing/:listingId" element={<Edit />} />
-          </Route>
 
-          <Route path="*" element={<NotFound />} /> */}
+          <Route path="/forgot-password" element={<ForgotPasswordPage></ForgotPasswordPage>}
+          />
+          <Route path="*" element={<NotFound />} />
+        
         </Routes>
 
       </Router>
---
+++ src/pages/ForgotPasswordPage.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from 'react-router-dom'
+import './ForgotPasswordPage.scss'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Card from 'react-bootstrap/Card';
+import { FcGoogle } from 'react-icons/fc';
+import { toast } from "react-toastify";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import GoogleAuthBtnComp from "../components/GoogleAuthBtnComp";
+
+export default function ForgotPasswordPage() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, } = formData;
+
+    function onChange(e) {
+        setFormData((prevState) => ({
+            ...prevState,
+
+            [e.target.id]: e.target.value,
+        }))
+    }
+    async function onForgotPassword(e) {
+        e.preventDefault();
+        try {
+            const auth = getAuth();
+            await sendPasswordResetEmail(auth, email);
+            toast.success('Email was sent')
+        } catch (error) {
+            toast.error('Could not send reset password')
+        }
+    }
+    return (
+        <section>
+            <h1 className="title">Forgot password</h1>
+            <div className="main-container" >
+                <div className="image-container" >
+                    <Card className="bg-dark text-white">
+                        <Card.Img className="image-container-image" src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                            alt="key"
+                        />
+                    </Card>
+                </div>
+                <div className="form-container" >
+                    <Form onSubmit={onForgotPassword}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                label="Email address"
+                                className="mb-3"
+                            >
+                                <Form.Control
+                                    type="email"
+                                    placeholder="name@example.com"
+                                    controlId="email"
+                                    className="form-input"
+                                    value={email}
+                                    required
+                                    onChange={onChange}
+                                />
+                            </FloatingLabel>
+
+                            <FloatingLabel className="forgot-password-container">
+                                <span>Don't have an account?<span><Link style={{ color: 'red', textDecoration: 'none' }} to='/sign-in' > Register</Link></span> </span>
+                                <span> <Link style={{ color: 'blue', textDecoration: 'none' }} to='/sign-in' >Sign in instead</Link></span>
+                            </FloatingLabel>
+                        </Form.Group>
+                        <Button variant="primary" type="submit" className="submit-btn">
+                            SEND RESET EMAIL
+                        </Button>
+                        <GoogleAuthBtnComp></GoogleAuthBtnComp>
+                    </Form>
+                </div>
+            </div>
+        </section>
+    );
+}
